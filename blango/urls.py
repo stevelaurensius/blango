4cc5fdaca4260b3da90c02e7bd87ebc3287f7d8a
Steven Steven (Steve Laurensius) <steve.laurensius@gmail.com>
2026-01-23 16:42:13 +0000

    Finish First-Party Django REST API

--- blango/urls.py
+++ blango/urls.py
@@ -35,4 +35,5 @@ urlpatterns = [
     ),
     path("accounts/", include("django_registration.backends.activation.urls")),
     path("accounts/", include("allauth.urls")),
+    path("api/v1/", include("blog.api_urls")),
 ]
