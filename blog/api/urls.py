375ed4b100684d68e692c59c679edaaa31d5d17f
Steven Steven (Steve Laurensius) <steve.laurensius@gmail.com>
2026-01-27 09:14:36 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
-from blog.api.views import PostList, PostDetail
+from blog.api.views import PostList, PostDetail, UserDetail
+from blango_auth.models import User
+
+# urlpatterns = [
+#     path("posts/", PostList.as_view(), name="api_post_list"),
+#     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+#     path("auth/", include("rest_framework.urls")),
+#     path("token-auth/", views.obtain_auth_token),
+# ]
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
-    path("auth/", include("rest_framework.urls")),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     path("token-auth/", views.obtain_auth_token),
 ]
 
